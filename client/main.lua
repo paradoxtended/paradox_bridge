ed1270ad90a151a9dbe046a3a306cc1eba8485cf
Timothy Barabas <215121906+theravaget@users.noreply.github.com>
2025-09-09 20:37:11 +0200

    chore: exportable editables

--- client/main.lua
+++ client/main.lua
@@ -1,1 +1,12 @@
 exports('getUtils', function() return Utils end)
+
+local LR = {
+    notify = ShowNotification,
+    showUI = ShowUI,
+    hideUI = HideUI,
+    progressBar = ShowProgressBar,
+    isProgressActive = IsProgressActive,
+    cancelProgress = CancelProgress
+}
+
+exports('getEditables', function() return LR end)
